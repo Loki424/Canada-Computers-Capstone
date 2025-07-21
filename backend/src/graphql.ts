8c4ecf0075f35b0d3485fa39f77272efe35d239a
Loki424 <147016381+Loki424@users.noreply.github.com>
2025-07-20 22:39:06 -0400

    Update graphql.ts

--- backend/src/graphql.ts
+++ backend/src/graphql.ts
@@ -1,19 +1,12 @@
-// api/graphql.ts
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import serverless from 'serverless-http';
-import { buildApp } from '../src/app';
+import { buildApp } from './app';
 
-const appPromise = buildApp();
-let handler: ReturnType<typeof serverless> | null = null;
-
-// this function is invoked on every Vercel request
 export default async function vercelHandler(
   req: VercelRequest,
   res: VercelResponse
 ) {
-  if (!handler) {
-    const app = await appPromise;
-    handler = serverless(app);
-  }
+  const app = await buildApp();
+  const handler = serverless(app);
   return handler(req, res);
 }
